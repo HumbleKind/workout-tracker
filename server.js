450b199486081c0a3e5942a9b6946b3a7e87004e
Christian Sinai <christian_sinai@me.com>
2020-11-09 17:05:13 -0800

    initial commit, with mongodb atlas connection

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+);
+
+// QUESTION: is this 'route' required?
+app.use(require("./routes/api"));
+
+app.listen(PORT, () => {
+    console.log("App running on port " + PORT + "!");
+});
