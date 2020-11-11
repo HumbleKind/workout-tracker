27c05052836e487587bcc4e8d29edcd3c2ce461e
Christian Sinai <christian_sinai@me.com>
2020-11-11 09:46:41 -0800

    added api/ html routing

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(
     }
 );
 
-// QUESTION: is this 'route' required?
-// app.use(require("./routes/api"));
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
     console.log("App running on port " + PORT + "!");
