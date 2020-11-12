2b98c149d2593143898630f320dc9e4143bc62d6
Christian Sinai <christian_sinai@me.com>
2020-11-11 16:05:41 -0800

    cleaned up white space

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,34 +17,34 @@ router.put("/api/workouts/:id", ({body, params}, res) => {
         { $push: { exercises: body } },
         { new: true }
         )
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 router.get("/api/workouts/range", (req, res) => {
     Workout.find({})
-      .sort({ date: -1 })
-      .limit(7)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
+        .sort({ date: -1 })
+        .limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
-  module.exports = router;
+module.exports = router;
